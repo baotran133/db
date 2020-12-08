445b78dfa6b142d90f268a5f1678554b87d98de3
baotran133 <58819267+baotran133@users.noreply.github.com>
2020-12-08 22:39:30 +0700

    web

--- includes/permission_manageaccount.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['loggedin']) == false) {
-	// Nếu người dùng chưa đăng nhập thì chuyển hướng website sang trang đăng nhập
-	header('Location: login.html');
-}
-?>
